6cc68cfbe7fe56df43912122068271cea21ac869
Wolfram Huesken <woh@m18.io>
2016-11-06 16:54:27 +0800

    Replaced Tomcat with Undertow for no reason Fixed typing bug

--- src/main/java/org/m18/pix/models/repositories/ImageRepository.java
+++ src/main/java/org/m18/pix/models/repositories/ImageRepository.java
@@ -2,12 +2,12 @@ package org.m18.pix.models.repositories;
 
 import javax.transaction.Transactional;
 import org.m18.pix.models.entities.Image;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Component;
-import org.springframework.data.repository.CrudRepository;
 
 /**
  * @author Wolfram Huesken <woh@m18.io>
  */
 @Component
 @Transactional(rollbackOn = Exception.class)
-public interface ImageRepository extends CrudRepository<Image, Integer> {}
+public interface ImageRepository extends PagingAndSortingRepository<Image, Long> {}
